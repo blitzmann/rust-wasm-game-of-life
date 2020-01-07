c3f7cb3803849094b3016653b5908c99e298a734
blitzmann <holmes.ryan.90@gmail.com>
2020-01-07 00:06:22 -0500

    Performance tuning

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate js_sys;
+extern crate web_sys;
 mod utils;
 use wasm_bindgen::prelude::*;
 extern crate fixedbitset;
@@ -12,7 +13,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn init_panic_hook() {
-    console_error_panic_hook::set_once();
+    // console_error_panic_hook::set_once();
 }
 
 #[wasm_bindgen]
@@ -30,6 +31,34 @@ pub struct Universe {
     cells: FixedBitSet,
     gen: u32
 }
+
+// use web_sys::console;
+
+// pub struct Timer<'a> {
+//     name: &'a str,
+// }
+
+// impl<'a> Timer<'a> {
+//     pub fn new(name: &'a str) -> Timer<'a> {
+//         console::time_with_label(name);
+//         Timer { name }
+//     }
+// }
+
+// impl<'a> Drop for Timer<'a> {
+//     fn drop(&mut self) {
+//         console::time_end_with_label(self.name);
+//     }
+// }
+
+
+// fn now() -> f64 {
+//     web_sys::window()
+//         .expect("should have a Window")
+//         .performance()
+//         .expect("should have a Performance")
+//         .now()
+// }
 
 /// Public methods, exported to JavaScript.
 #[wasm_bindgen]
@@ -87,22 +116,74 @@ impl Universe {
 
     fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
         let mut count = 0;
-        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-                if delta_row == 0 && delta_col == 0 {
-                    continue;
-                }
+        
+        // // expensive way
+        // for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+        //     for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+        //         if delta_row == 0 && delta_col == 0 {
+        //             continue;
+        //         }
 
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col = (column + delta_col) % self.width;
-                let idx = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[idx] as u8;
-            }
-        }
+        //         let neighbor_row = (row + delta_row) % self.height;
+        //         let neighbor_col = (column + delta_col) % self.width;
+        //         let idx = self.get_index(neighbor_row, neighbor_col);
+        //         count += self.cells[idx] as u8;
+        //     }
+        // }
+
+        let north = if row == 0 {
+            self.height - 1
+        } else {
+            row - 1
+        };
+
+        let south = if row == self.height - 1 {
+            0
+        } else {
+            row + 1
+        };
+
+        let west = if column == 0 {
+            self.width - 1
+        } else {
+            column - 1
+        };
+
+        let east = if column == self.width - 1 {
+            0
+        } else {
+            column + 1
+        };
+
+        let nw = self.get_index(north, west);
+        count += self.cells[nw] as u8;
+
+        let n = self.get_index(north, column);
+        count += self.cells[n] as u8;
+
+        let ne = self.get_index(north, east);
+        count += self.cells[ne] as u8;
+
+        let w = self.get_index(row, west);
+        count += self.cells[w] as u8;
+
+        let e = self.get_index(row, east);
+        count += self.cells[e] as u8;
+
+        let sw = self.get_index(south, west);
+        count += self.cells[sw] as u8;
+
+        let s = self.get_index(south, column);
+        count += self.cells[s] as u8;
+
+        let se = self.get_index(south, east);
+        count += self.cells[se] as u8;
+
         count
     }
     
     pub fn tick(&mut self) {
+        // let _timer = Timer::new("Universe::tick");
         let mut next = self.cells.clone();
 
         for row in 0..self.height {
@@ -114,21 +195,25 @@ impl Universe {
                 let next_cell = match (cell, live_neighbors) {
                     // Rule 1: Any live cell with fewer than two live neighbours
                     // dies, as if caused by underpopulation.
-                    (true, x) if x < 2 => false,
+                    (true, x) if x < 2 => (false, true),
                     // Rule 2: Any live cell with two or three live neighbours
                     // lives on to the next generation.
-                    (true, 2) | (true, 3) => true,
+                    (true, 2) | (true, 3) => (true, true),
                     // Rule 3: Any live cell with more than three live
                     // neighbours dies, as if by overpopulation.
-                    (true, x) if x > 3 => false,
+                    (true, x) if x > 3 => (false, true),
                     // Rule 4: Any dead cell with exactly three live neighbours
                     // becomes a live cell, as if by reproduction.
-                    (false, 3) => true,
+                    (false, 3) => (true, true),
                     // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
+                    (otherwise, _) => (otherwise, false)
                 };
 
-                next.set(idx, next_cell);
+                // todo: keep track of deltas
+                // if next_cell.1 {
+                //     next.set(idx, next_cell.0);
+                // }
+                next.set(idx, next_cell.0);
             }
         }
         self.gen += 1;
--- www/index.js
+++ www/index.js
@@ -10,7 +10,7 @@ const ALIVE_COLOR = "#000000";
 
 const gen = document.getElementById("generation");
 
-const universe = Universe.new(100, 100, true);
+const universe = Universe.new(128, 128, true);
 const width = universe.width();
 const height = universe.height();
 
@@ -23,6 +23,49 @@ canvas.width = (CELL_SIZE + 1) * width + 1;
 const ctx = canvas.getContext('2d');
 
 let playToggle = false;
+
+const fps = new class {
+    constructor() {
+      this.fps = document.getElementById("fps");
+      this.frames = [];
+      this.lastFrameTimeStamp = performance.now();
+    }
+  
+    render() {
+      // Convert the delta time since the last frame render into a measure
+      // of frames per second.
+      const now = performance.now();
+      const delta = now - this.lastFrameTimeStamp;
+      this.lastFrameTimeStamp = now;
+      const fps = 1 / delta * 1000;
+  
+      // Save only the latest 100 timings.
+      this.frames.push(fps);
+      if (this.frames.length > 100) {
+        this.frames.shift();
+      }
+  
+      // Find the max, min, and mean of our 100 latest timings.
+      let min = Infinity;
+      let max = -Infinity;
+      let sum = 0;
+      for (let i = 0; i < this.frames.length; i++) {
+        sum += this.frames[i];
+        min = Math.min(this.frames[i], min);
+        max = Math.max(this.frames[i], max);
+      }
+      let mean = sum / this.frames.length;
+  
+      // Render the statistics.
+      this.fps.textContent = `
+  Frames per Second:
+           latest = ${Math.round(fps)}
+  avg of last 100 = ${Math.round(mean)}
+  min of last 100 = ${Math.round(min)}
+  max of last 100 = ${Math.round(max)}
+  `.trim();
+    }
+  };
 
 const renderLoop = () => {
     if (playToggle) {
@@ -51,7 +94,11 @@ const drawGrid = () => {
 };
 
 const doTick = () => {
-    universe.tick();
+    fps.render(); //new
+
+    for (let i = 0; i < 9; i++) {
+        universe.tick();
+      }
     gen.textContent = universe.gen();
 
     //drawGrid();
@@ -70,19 +117,36 @@ const bitIsSet = (n, arr) => {
 
 const drawCells = () => {
     const cellsPtr = universe.cells();
-    debugger;
+
     const cells = new Uint8Array(memory.buffer, cellsPtr, (width * height) / 8);
 
     ctx.beginPath();
 
+    ctx.fillStyle = ALIVE_COLOR;
     for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
             const idx = getIndex(row, col);
 
-            ctx.fillStyle = !bitIsSet(idx, cells)
-                ? DEAD_COLOR
-                : ALIVE_COLOR;
+            if (!bitIsSet(idx, cells)) {
+                continue;
+              }
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
 
+    ctx.fillStyle = DEAD_COLOR;
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+
+            if (bitIsSet(idx, cells)) {
+                continue;
+              }
             ctx.fillRect(
                 col * (CELL_SIZE + 1) + 1,
                 row * (CELL_SIZE + 1) + 1,
